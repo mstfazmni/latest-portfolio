95416e87596a6c7e7e31d7550a28db18baf5ff1b
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-02-04 10:33:41 -0700

    Navbar styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AnimatePresence } from "framer-motion"; // Import AnimatePresence
+import { AnimatePresence } from "framer-motion"; //smooth scrolling
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
+import '../styles/Navbar.css';
+import myLogo from '../assets/mz-f-w.png';
 
 const Navbar = () => {
     return(
-        <nav className="navbar-class">
+        <nav className="navbar-container">
+            
+            <section className="left-sec">
+            <img src={myLogo} alt="LOGO" className="my-logo"></img>
+            </section>
+
+            <section className="right-sec">
             <ul className="ul-calss">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/works">Works</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
+                <li> <Link to="#">Resume</Link></li>
             </ul>
+            </section>
+          
         </nav>
     );
 }
