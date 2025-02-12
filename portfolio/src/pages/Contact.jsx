467b6f8d711e4876020b9acafb9bb6ccf61f03de
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-02-12 09:50:49 -0700

    contact logos and email in right section

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
 import '../styles/Contact.css';
+import gitLogo from '../assets/github.png';
+import linkedLogo from '../assets/linkedin.png';
 
 const Contact = () => {
   return (
@@ -31,6 +33,20 @@ const Contact = () => {
           </section>
 
           <section className="contact-right-sec">
+              <div className="logos-email-container">
+                  <div className="logos-div">
+                      <a href="https://github.com/mstfazmni" target="_blank" rel="noopener noreferrer">
+                        <img src={gitLogo} alt="GitHub" className="git-logo" />
+                      </a>
+                      <a href="https://www.linkedin.com/in/mo-zamani" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedLogo} alt="LinkedIn" className="linked-logo" />
+                      </a>
+                  </div>
+                  <div className="email-div">
+                    <p>You can also email me</p>
+                    <a href="mailto:zmnimstfa@gmail.com">zmnimstfa@gmail.com</a>
+                  </div>
+              </div>
           </section>
         </div>
         
