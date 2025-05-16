b825704ce65e5e2a996146495e34179ca3c4a586
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-05-16 00:08:22 -0600

    Updated |

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import { useLocation, Link } from "react-router-dom";
 import "../styles/Navbar.css";
-import myLogo from "../assets/mz-f-w.png";
+import myLogoBlack from "../assets/mz-f-w.png";
+import myLogoWhite from "../assets/mz-f-b.png";
+
 
 const Navbar = () => {
   const location = useLocation();
 
-  // Define paths where background is dark
-  const darkRoutes = ["/works", "/project/:id"]; 
-  const isDark = darkRoutes.some(path => location.pathname.startsWith(path.replace(":id", "")));
+  // Logo should be white on these dark background routes
+  const logoDarkRoutes = ["/works", "/project", "/contact"];
+  const isDarkLogo = logoDarkRoutes.some(path => location.pathname.startsWith(path));
+
+  // Text should be white only on these routes
+  const textWhiteRoutes = ["/works", "/project"];
+  const isWhiteText = textWhiteRoutes.some(path => location.pathname.startsWith(path));
 
   return (
-    <nav className={`navbar-container ${isDark ? "navbar-dark" : "navbar-light"}`}>
+    <nav className={`navbar-container ${isWhiteText ? "navbar-dark" : ""}`}>
       <section className="left-sec">
-        <img src={myLogo} alt="LOGO" className="my-logo" />
+        <img src={isDarkLogo ? myLogoWhite : myLogoBlack} alt="LOGO" className="my-logo" />
       </section>
 
       <section className="right-sec">
-        <ul className="ul-calss">
-          <li><Link to="/" className={isDark ? "white-text" : ""}>Home</Link></li>
-          <li><Link to="/about" className={isDark ? "white-text" : ""}>About</Link></li>
-          <li><Link to="/works" className={isDark ? "white-text" : ""}>Works</Link></li>
-          <li><Link to="/contact" className={isDark ? "white-text" : ""}>Contact</Link></li>
+         <ul className="ul-calss">
+          <li><Link to="/" className={isWhiteText ? "white-text" : ""}>Home</Link></li>
+          <li><Link to="/about" className={isWhiteText ? "white-text" : ""}>About</Link></li>
+          <li><Link to="/works" className={isWhiteText ? "white-text" : ""}>Works</Link></li>
+          <li><Link to="/contact" className={isWhiteText ? "white-text" : ""}>Contact</Link></li>
         </ul>
       </section>
     </nav>
